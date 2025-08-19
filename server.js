8c78ce8ec384f6ee1266ff694bde09e1ced8ba89
thequietworld <155055938+thequietworld@users.noreply.github.com>
2025-08-19 16:48:09 +0930

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const multer  = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const upload = multer({ dest: 'uploads/' }); // folder to store images
 
-// Serve static files (frontend page)
-app.use(express.static('public'));
-
-// Receive uploaded images from iPhone
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log('Received:', req.file.originalname);
-  // Save latest image as latest.jpg
-  const fs = require('fs');
-  fs.copyFileSync(req.file.path, path.join(__dirname, 'public', 'latest.jpg'));
-  res.send('Uploaded!');
-});
-
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
-const express = require('express');
-const app = express();
-const path = require('path');
+// Folder to store uploaded images temporarily
+const upload = multer({ dest: 'uploads/' });
 
 // Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve index.html on root
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Receive uploaded images from iPhone
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log('Received:', req.file.originalname);
+    // Save latest image as latest.jpg in public folder
+    fs.copyFileSync(req.file.path, path.join(__dirname, 'public', 'latest.jpg'));
+    res.send('Uploaded!');
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
